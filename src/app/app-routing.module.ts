84bd5e90cbd02f2fced4f516c952ea03754d5e49
JayeshShingote92 <jayeshshingote92@gmail.com>
2023-01-28 16:07:39 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../app/pages/home/home.component';
+import { AboutusComponent } from '../app/pages/aboutus/aboutus.component';
+import { ProductandservicesComponent } from '../app/pages/productandservices/productandservices.component';
+import { ContactComponent } from '../app/pages/contact/contact.component';
+import { CareerComponent } from '../app/pages/career/career.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutusComponent },
+  { path: 'productadservices', component: ProductandservicesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'career', component: CareerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
